5e2847440c0a4204e91e5013262ed134d9554620
Ben Konyi <bkonyi@google.com>
2017-06-16 13:11:37 -0700

    Updated licensing information, added Google clause to LICENSE, added disclaimer that this is not an official Google product, and moved files to their correct locations.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ DRAW: The Dart Reddit API Wrapper
 [![Build Status](https://travis-ci.org/draw-dev/DRAW.svg?branch=master)](https://travis-ci.org/draw-dev/DRAW/)
 
 Check back soon for more info!
+
+Disclamer: This is not an official Google product.
---
+++ lib/src/reddit.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2017, the Dart Reddit API Wrapper  project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that 
+// can be found in the LICENSE file.
+
+/// An object that with the future ability to provide access to user data and subreddits along with HTTP capablities.
+///
+///Future private members include A Config Object, User Data Object, and much more!
+class Reddit {
+  Reddit() {
+    ///This is currently a placeholder for the Reddit Constructor.
+  }
+}
