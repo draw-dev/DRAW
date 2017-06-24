c92005c4ccf0da4e6b72f3c370a3ed7941a4d036
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-06-24 02:42:13 -0400

    Added file to contain drawConfigContext

---
+++ draw/drawConfigContext.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2017, krishchopra. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+import 'package:ini/ini.dart';
+
+
+class DrawConfigContext{
+
+  DrawConfigContext();
+  
+  Future<file> loadConfig(){}
+
+}
