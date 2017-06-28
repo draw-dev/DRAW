eb36e3c016c047fccb099e083013e05117a5cb06
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-06-28 16:41:03 -0400

    Added the beginnings of the dart config context and modified .gitignore

--- draw/drawConfigContext.dart
+++ draw/drawConfigContext.dart
@@ -4,11 +4,27 @@
 import 'dart:io';
 import 'package:ini/ini.dart';
 
+import 'exeptions.dart';
+
 
 class DrawConfigContext{
 
   DrawConfigContext();
-  
-  Future<file> loadConfig(){}
+
+  Map<String, String> customDataStructure;
+
+  String shortURL;
+  String settings;
+  String clientId;
+  String redditURL;
+  String password;
+
+  void _loadConfig(){
+    //TODO: Kartik (kc3454) impment
+
+    throw new UnimplmentedError;
+  }
+
+
 
 }
