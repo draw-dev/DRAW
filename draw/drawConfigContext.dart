85cbb7a64f9be2b954099a1382fcb04bfcde4786
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-07-11 14:20:28 -0400

    changed autogenerated copyright comment to Ben's

--- draw/drawConfigContext.dart
+++ draw/drawConfigContext.dart
@@ -1,5 +1,7 @@
-// Copyright (c) 2017, krishchopra. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2017, the Dart Reddit API Wrapper  project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
 
 import 'dart:io';
 import 'package:ini/ini.dart';
@@ -58,7 +60,7 @@ class DrawConfigContext{
     }
     return osConfigPath;
   }
-
+w
   //Returns path to global configuration file
   Uri _getGlobalConfigPath(){
     path.Context context = new path.Context();
