541af3e5e1fd0a22b294e4ae03fed0f614373ad9
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-08-11 12:42:32 -0400

    Minor changes to comments and added bool safety check function

--- lib/src/drawConfigContext.dart
+++ lib/src/drawConfigContext.dart
@@ -1,7 +1,7 @@
-// Copyright (c) 2017, the Dart Reddit API Wrapper  project authors.
-// Please see the AUTHORS file for details. All rights reserved.
-// Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
+/// Copyright (c) 2017, the Dart Reddit API Wrapper  project authors.
+/// Please see the AUTHORS file for details. All rights reserved.
+/// Use of this source code is governed by a BSD-style license that
+/// can be found in the LICENSE file.
 
 import 'dart:io';
 import 'package:ini/ini.dart';
@@ -17,7 +17,7 @@ const String kLinuxEnvVar = 'XDG_CONFIG_HOME';
 const String kWindowsEnvVar = 'APPDATA';
 
 class DrawConfigContext {
-  //Path to Local, User, Global Config Files, with matching precedence
+  ///Path to Local, User, Global Config Files, with matching precedence
   Uri _localConfigPath;
   Uri _userConfigPath;
   Uri _globalConfigPath;
@@ -36,24 +36,25 @@ class DrawConfigContext {
   String _redirectURI;
   String _refreshToken;
   String _redditURL;
-  
-  DrawConfigContext() {
-    //Get file paths
-    this._localConfigPath = this._getLocalConfigPath();
-    this._userConfigPath = this._getUserConfigPath();
-    this._globalConfigPath = this._getGlobalConfigPath();
 
-    //Check for file existence
+  DrawConfigContext() {
+    ///Get file paths
+    _localConfigPath  = _getLocalConfigPath();
+    _userConfigPath   = _getUserConfigPath();
+    _globalConfigPath = _getGlobalConfigPath();
+
+    ///Check for file existence
     File primaryFile = new File(this._localConfigPath.toString());
 
     if (primaryFile.exists() == false) {
       primaryFile = new File(this._userConfigPath.toString());
     }
+
     if (primaryFile.exists() == false) {
       primaryFile = new File(this._globalConfigPath.toString());
     }
 
-    //Parse file
+    ///Parse file
     primaryFile
         .readAsLines()
         .then((lines) => new Config.fromStrings(lines))
@@ -70,46 +71,53 @@ class DrawConfigContext {
           print(e);
         });
 
-    //TODO: Load settings into master data-structure Settings
-    //TODO: Load main file into configFile
-    //this._config = new Config.fromStrings(this.configFile.readAsLinesSync());
+    ///TODO: Load settings into master data-structure Settings
+    ///TODO: Load main file into configFile
+    ///this._config = new Config.fromStrings(this.configFile.readAsLinesSync());
   }
-
 
   void _initializeAttributes() {
-    //Fetch Default
-    _shortUrl		= _fetchDefault('short_url');
-    _checkForUpdates 	= _configBool(_fetchDefault('check_for_updates'));
-    
-    //Fetch
-    _oauthUrl 		= _fetch('oauth_url');
-    _redditUrl		= _fetch('reddit_url');
-    
-    //Fetch_or_not_set_
-    _clientId		= _fetchOrNotSet('client_id');
-    _clientSecret	= _fetchOrNotSet('client_secret');
-    _httpProxy		= _fetchOrNotSet('http_proxy');
-    _httpsProxy		= _fetchOrNotSet('https_proxy');
-    _redirectUri   	= _fetchOrNotSet('redirect_uri');
-    _refreshToken       = _fetchOrNotSet('refresh_token');
-    _password		= _fetchOrNotSet('password');
-    _userAgent		= _fetchOrNotSet('user_agent');
-    _username 		= _fetchOrNotSet('username');
+    ///Fetch Default
+    _shortUrl = _fetchDefault('short_url');
+    _checkForUpdates = _configBool(_fetchDefault('check_for_updates'));
 
+    ///Fetch
+    _oauthUrl = _fetch('oauth_url');
+    _redditUrl = _fetch('reddit_url');
 
-
+    ///Fetch_or_not_set_
+    _clientId = _fetchOrNotSet('client_id');
+    _clientSecret = _fetchOrNotSet('client_secret');
+    _httpProxy = _fetchOrNotSet('http_proxy');
+    _httpsProxy = _fetchOrNotSet('https_proxy');
+    _redirectUri = _fetchOrNotSet('redirect_uri');
+    _refreshToken = _fetchOrNotSet('refresh_token');
+    _password = _fetchOrNotSet('password');
+    _userAgent = _fetchOrNotSet('user_agent');
+    _username = _fetchOrNotSet('username');
   }
 
-  String _fetch_default(String key) {
+  ///Safely return the truth value associated with [item].
+  bool _configBool(var item) {
+    if (item is bool) {
+      return item;
+    } else {
+      const Set<String> trueValues = ['1', 'yes', 'true', 'on'];
+      return trueValues.contains(item);
+    }
+  }
+
+  String _fetchDefault(String key) {
     return this._customConfig.get("default", key);
   }
 
-  //Returns path to user level configuration file
+  ///Returns path to user level configuration file
   Uri _getUserConfigPath() {
     final Map<String, String> environ = Platform.environment;
 
     Uri osConfigPath;
-    //Load correct config path based on operating system
+
+    ///Load correct config path based on operating system
     if (Platform.isMacOS) {
       osConfigPath = Uri.parse(path.join(environ[kMacEnvVar], '.config'));
     } else if (Platform.isLinux) {
@@ -120,19 +128,19 @@ class DrawConfigContext {
     return osConfigPath;
   }
 
-  //Returns path to global configuration file
+  ///Returns path to global configuration file
   Uri _getGlobalConfigPath() {
     final path.Context context = new path.Context();
     final String cwd = context.current;
     return Uri.parse(path.join(cwd, kFileName));
   }
 
-  //Returns path to local Configuration file
+  ///Returns path to local Configuration file
   Uri _getLocalConfigPath() {
     return Uri.parse(kFileName);
   }
 
-  //TODO: Kartik implment (kc3454)
+  ///TODO: Kartik implment (kc3454)
   String shortURL() {
     return this._shortURL;
   }
