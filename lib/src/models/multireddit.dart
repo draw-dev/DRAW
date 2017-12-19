9394e1260d0735bd3328649677f0d758933504c6
Kartik Chopra <k5chopra@uwaterloo.ca>
2017-12-19 15:37:33 -0500

    Implmented copy, delete, rename, remove

--- lib/src/models/multireddit.dart
+++ lib/src/models/multireddit.dart
@@ -44,7 +44,7 @@ class Multireddit extends RedditBase
   final RegExp _subredditRegExp = new RegExp(r'{subreddit}');
   final RegExp _userRegExp = new RegExp(r'{user}');
 
-  ///Returns a slug versio of the title
+  ///Returns a slug version of the title
   static String sluggify(String title) {
     title = title.replaceAll(_invalidRegExp, '_').trim();
     if (title.length > 21) {
@@ -60,6 +60,13 @@ class Multireddit extends RedditBase
   Multireddit.parse(Reddit reddit, Map data)
       : super.loadData(reddit, data['data']);
 
+  ///TODO(k5chopra): Research if this would be better off, pre-computed
+  String _info_path() {
+    return apiPath['multireddit_api']
+        .replaceAll(_multiredditRegExp, _name)
+        .replaceAll(_userRegExp, _authorName);
+  }
+
   void add(String subreddit) {
     String url = apiPath['multireddit_update']
         .replaceAll(_userRegExp, _authorName)
@@ -68,4 +75,48 @@ class Multireddit extends RedditBase
     var data = "I DONT KNOW WHAT TO PUT HERE YET";
     reddit.put(url, data);
   }
+
+  Multireddit copy([String display_name = null]) {
+    String name;
+    String url = apiPath['multireddit_copy'];
+    if (display_name != null) {
+      name = sliggify(display_name);
+    } else {
+      display_name = _name;
+      name = _name;
+    }
+    data = const {
+      kDisplayName: display_name,
+      kFrom: _path,
+      kTo: apiPath['multiredit']
+          .replaceAll(_multiredditRegExp, name)
+          .replaceAll(_userRegExp, reddit.user.me()),
+    };
+    return reddit.post(url, data);
+  }
+
+  ///Delete this [multireddit]
+  void delete() {
+    reddit.request(_info_path());
+  }
+
+  void remove(String subreddit) {
+    string url = apiPath['multireddit_update']
+        .replaceAll(_multiredditRegExp, _name)
+        .replaceAll(_userRegExp, _author)
+        .replaceAll(_subredditRegExp, subreddit);
+    data = "Need to do this part";
+    reddit.delete(url, data);
+  }
+
+  void rename(display_name) {
+    String url = apiPath['multireddit_rename'];
+    data = {
+      kFrom: _path,
+      kDisplayName: display_name,
+    };
+    updated = reddit.post(url, data);
+  }
+
+  //Need to implment Update
 }
