3798c34184dba7ae478a9f6ebc2eb9b74db0d49f
Kartik Chopra <k5chopra@uwaterloo.ca>
2018-04-15 00:32:26 -0400

    Add delete method, fields, tests to multireddit
    New Fields Added to multireddit include:
            - canEdit
            - createdUTC
            - subreddits (Returned as a List of SubredditRefs!)
            - displayName (From map)
            - visibility
            - weightingScheme
            - iconName
            - keyColor
            - over18
    
    Adds Delete delete method to delete valid multireddit within user roles.
    
    Added corresponding tests for new fields and the delete method.

--- test/multireddit/multireddit_test.dart
+++ test/multireddit/multireddit_test.dart
@@ -6,52 +6,51 @@ import 'dart:async';
 
 import 'package:draw/draw.dart';
 import 'package:test/test.dart';
+import 'package:color/color.dart';
 
 import '../test_utils.dart';
 
 Future main() async {
-
-    final expectedSubredditList = [
-      {"name": "solotravel"},
-      {"name": "Fishing"},
-      {"name": "geocaching"},
-      {"name": "Yosemite"},
-      {"name": "CampingandHiking"},
-      {"name": "whitewater"},
-      {"name": "remoteplaces"},
-      {"name": "Adirondacks"},
-      {"name": "caving"},
-      {"name": "travelphotos"},
-      {"name": "canyoneering"},
-      {"name": "Backcountry"},
-      {"name": "Bushcraft"},
-      {"name": "Kayaking"},
-      {"name": "scuba"},
-      {"name": "bouldering"},
-      {"name": "CampingGear"},
-      {"name": "urbanexploration"},
-      {"name": "WWOOF"},
-      {"name": "adventures"},
-      {"name": "climbing"},
-      {"name": "Mountaineering"},
-      {"name": "sailing"},
-      {"name": "hiking"},
-      {"name": "iceclimbing"},
-      {"name": "tradclimbing"},
-      {"name": "backpacking"},
-      {"name": "trailmeals"},
-      {"name": "Climbingvids"},
-      {"name": "camping"},
-      {"name": "climbharder"},
-      {"name": "skiing"},
-      {"name": "Outdoors"},
-      {"name": "alpinism"},
-      {"name": "PacificCrestTrail"},
-      {"name": "Ultralight"},
-      {"name": "Hammocks"},
-      {"name": "AppalachianTrail"}
-    ];
-
+  final expectedSubredditList = [
+    {"name": "solotravel"},
+    {"name": "Fishing"},
+    {"name": "geocaching"},
+    {"name": "Yosemite"},
+    {"name": "CampingandHiking"},
+    {"name": "whitewater"},
+    {"name": "remoteplaces"},
+    {"name": "Adirondacks"},
+    {"name": "caving"},
+    {"name": "travelphotos"},
+    {"name": "canyoneering"},
+    {"name": "Backcountry"},
+    {"name": "Bushcraft"},
+    {"name": "Kayaking"},
+    {"name": "scuba"},
+    {"name": "bouldering"},
+    {"name": "CampingGear"},
+    {"name": "urbanexploration"},
+    {"name": "WWOOF"},
+    {"name": "adventures"},
+    {"name": "climbing"},
+    {"name": "Mountaineering"},
+    {"name": "sailing"},
+    {"name": "hiking"},
+    {"name": "iceclimbing"},
+    {"name": "tradclimbing"},
+    {"name": "backpacking"},
+    {"name": "trailmeals"},
+    {"name": "Climbingvids"},
+    {"name": "camping"},
+    {"name": "climbharder"},
+    {"name": "skiing"},
+    {"name": "Outdoors"},
+    {"name": "alpinism"},
+    {"name": "PacificCrestTrail"},
+    {"name": "Ultralight"},
+    {"name": "Hammocks"},
+    {"name": "AppalachianTrail"}
+  ];
 
   test('lib/multireddit/copy', () async {
     final reddit = await createRedditTestInstance(
@@ -70,5 +69,45 @@ Future main() async {
     final newMulti = await multireddit.copy('test-copy-adventure-2');
     expect(newMulti.displayName, 'test_copy_adventure_2');
     expect(newMulti.subreddits, multireddit.subreddits);
+  });
+
+  test('lib/multireddit/userMultis', () async {
+    final reddit = await createRedditTestInstance(
+        'test/multireddit/lib_user_multireddits.json');
+    final multis = await reddit.user.multireddits();
+    expect(multis.length, equals(9));
+    final multi = multis[1];
+
+    // Testing using data variable.
+    expect(multi.data['name'], equals('drawtestingmulti'));
+    expect(multi.data['display_name'], equals('drawtestingmulti'));
+    expect(multi.data['can_edit'], isTrue);
+    expect(multi.data['subreddits'].length, equals(81));
+
+    // Testing using getters.
+    expect(multi.author.displayName,
+        await reddit.user.me().then((redditor) => redditor.displayName));
+    expect(multi.over18, false);
+    expect(multi.keyColor, new HexColor('#cee3f8'));
+    expect(multi.visibility, Visibility.public);
+    expect(multi.weightingScheme, WeightingScheme.classic);
+    expect(multi.iconName, null);
+    expect(multi.displayName, equals('drawtestingmulti'));
+    expect(multi.fullname, equals('drawtestingmulti'));
+    expect(multi.canEdit, isTrue);
+    expect(multi.subreddits.length, equals(81));
+//    expect(multi.data['subreddits'][0]['name'], equals('lisp'));
+  });
+
+  test('lib/multireddit/deleteMulti', () async {
+    final reddit = await createRedditTestInstance(
+      'test/multireddit/lib_multireddit_delete.json',
+    );
+    final multis = await reddit.user.multireddits();
+    expect(multis.length, equals(9));
+    await multis[1].delete();
+    final newMultis = await reddit.user.multireddits();
+    expect(newMultis.length, equals(8));
+//    await reddit.auth.writeRecording();
   });
 }
