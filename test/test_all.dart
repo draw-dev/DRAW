bbefb5ab12e9150a4e8a124df83e7177be96da98
bkonyi <bkonyi@google.com>
2018-04-24 19:58:47 -0700

    Attempting to enable code coverage

---
+++ test/test_all.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2018, the Dart Reddit API Wrapper project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'comment/comment_test.dart' as comment_test;
+import 'frontpage/frontpage_test.dart' as frontpage_test;
+import 'inbox/inbox_tests.dart' as inbox_test;
+import 'redditor/redditor_test.dart' as redditor_test;
+import 'submission/submission_test.dart' as submission_test;
+import 'subreddit/subreddit_test.dart' as subreddit_test;
+import 'subreddit/subreddit_moderation_test.dart' as subreddit_mod_test;
+import 'unit_tests/src/test_draw_config_context.dart' as draw_config_test;
+import 'user/user_test.dart' as user_test;
+import 'user_content/user_content_test.dart' as user_content_test;
+
+void main() {
+  comment_test.main();
+  frontpage_test.main();
+  //inbox_test.main();
+  redditor_test.main();
+  submission_test.main();
+  subreddit_test.main();
+  subreddit_mod_test.main();
+  draw_config_test.main();
+  user_test.main();
+  user_content_test.main();
+}
---
+++ tools/travis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
+  echo "Running coverage..."
+  dart_coveralls report \
+    --retry 2 \
+    --exclude-test-files \
+    --throw-on-error \
+    --throw-on-connectivity-error \
+    --debug \
+    test/test_all.dart
+  echo "Coverage complete."
+els
+  if [ -z ${COVERALLS_TOKEN+x} ]; then echo "COVERALLS_TOKEN is unset"; fi
+  if [ -z ${TRAVIS_DART_VERSION+x} ]; then
+    echo "TRAVIS_DART_VERSION is unset";
+  else
+    echo "TRAVIS_DART_VERSION is $TRAVIS_DART_VERSION";
+  fi
+
+  echo "Skipping coverage for this configuration."
+fi
