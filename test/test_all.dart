d686997e7e09be2ed30993c43af47d8afbdc758d
Ben Konyi <bkonyi@google.com>
2018-04-27 11:23:19 -0700

    Added tests for RisingListingMixin

--- test/test_all.dart
+++ test/test_all.dart
@@ -9,6 +9,7 @@ import 'comment/comment_test.dart' as comment_test;
 import 'frontpage/frontpage_test.dart' as frontpage_test;
 import 'inbox/inbox_test.dart' as inbox_test;
 import 'redditor/redditor_test.dart' as redditor_test;
+import 'rising_listing_mixin/rising_listing_mixin_test.dart' as rising_listing_mixin_test;
 import 'submission/submission_test.dart' as submission_test;
 import 'subreddit/subreddit_test.dart' as subreddit_test;
 import 'subreddit/subreddit_moderation_test.dart' as subreddit_mod_test;
@@ -20,6 +21,7 @@ void main() {
   comment_test.main();
   frontpage_test.main();
   inbox_test.main();
+  rising_listing_mixin_test.main();
   redditor_test.main();
   submission_test.main();
   subreddit_test.main();
