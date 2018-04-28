55886d254d7427a0925982928d4f52283e59ddf9
Ben Konyi <bkonyi@google.com>
2018-04-27 17:25:15 -0700

    Added test coverage for SubredditListingMixin and GildedListingMixin (#71)
    * Added test coverage for SubredditListingMixin and GildedListingMixin
    
    * Renamed UserContentMixin to UserContentInitialized

--- lib/src/listing/mixins/subreddit.dart
+++ lib/src/listing/mixins/subreddit.dart
@@ -7,8 +7,8 @@ import 'dart:async';
 
 import '../listing_generator.dart';
 import '../../base.dart';
+import '../../models/comment.dart';
 import '../../models/subreddit.dart';
-import '../../models/user_content.dart';
 import '../../reddit.dart';
 import 'gilded.dart';
 
@@ -26,11 +26,11 @@ abstract class SubredditListingMixin {
 
 class CommentHelper extends RedditBase with GildedListingMixin {
   String get path => _subreddit.path;
-  final Subreddit _subreddit;
+  final SubredditRef _subreddit;
   CommentHelper(this._subreddit) : super(_subreddit.reddit);
 
-  Stream<UserContent> call({Map params}) =>
-      ListingGenerator.generator<UserContent>(reddit, _path(),
+  Stream<Comment> call({Map params}) =>
+      ListingGenerator.generator<Comment>(reddit, _path(),
           limit: ListingGenerator.getLimit(params), params: params);
 
   String _path() => _subreddit.path + 'comments/';
---
+++ test/subreddit/subreddit_listing_mixin_test.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2018, the Dart Reddit API Wrapper project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:test/test.dart';
+import 'package:draw/draw.dart';
+
+import '../test_utils.dart';
+
+Future main() async {
+  test('lib/subreddit/subreddit_listing_mixin_sanity', () async {
+    final reddit = await createRedditTestInstance(
+        'test/subreddit/lib_subreddit_listing_mixin_sanity.json');
+    final subreddit = reddit.subreddit('funny');
+    await for (final content in subreddit.comments(params: {'limit': 10})) {
+      expect(content is Comment, isTrue);
+      expect(content.body, isNotNull);
+    }
+  });
+}
