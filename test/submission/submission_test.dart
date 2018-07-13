20a3b189579e157cad3cbdeae304475381e01ee0
Ben Konyi <bkonyi@google.com>
2018-07-13 15:54:49 -0700

    Increased Submission test coverage

--- test/submission/submission_test.dart
+++ test/submission/submission_test.dart
@@ -15,6 +15,76 @@ Future<void> main() async {
     return subreddit.newest().map<Submission>((u) => u as Submission);
   }
 
+  test('lib/submission/properties-sanity', () async {
+    final reddit = await createRedditTestInstance(
+        'test/submission/lib_submission_properties.json');
+    final submission = await reddit.submission(id: '7x6ew7').populate();
+    expect(submission.approvedAt,
+        DateTime.fromMillisecondsSinceEpoch(1524535384000));
+    expect(submission.approved, isTrue);
+    expect(submission.approvedBy.displayName, 'DRAWApiOfficial');
+    expect(submission.archived, isFalse);
+    expect(submission.authorFlairText, isNull);
+    expect(submission.bannedAt, isNull);
+    expect(submission.bannedBy, isNull);
+    expect(submission.brandSafe, isNull);
+    expect(submission.canGild, isFalse);
+    expect(submission.canModeratePost, isTrue);
+    expect(submission.clicked, isFalse);
+    expect(submission.createdUtc,
+        DateTime.fromMillisecondsSinceEpoch(1518491082000, isUtc: true));
+    expect(submission.distinguished, isNull);
+    expect(submission.domain, 'youtube.com');
+    expect(submission.downvotes, 0);
+    expect(submission.edited, isFalse);
+    expect(submission.gilded, 0);
+    expect(submission.hidden, isFalse);
+    expect(submission.hideScore, isFalse);
+    expect(submission.ignoreReports, isFalse);
+    expect(submission.isCrosspostable, isTrue);
+    expect(submission.isRedditMediaDomain, isFalse);
+    expect(submission.isSelf, isFalse);
+    expect(submission.isVideo, isFalse);
+    expect(submission.likes, isTrue);
+    expect(submission.locked, isFalse);
+    expect(submission.numComments, 2);
+    expect(submission.numCrossposts, 0);
+    expect(submission.over18, isFalse);
+    expect(submission.pinned, isFalse);
+    expect(submission.quarantine, isFalse);
+    expect(submission.removalReason, isNull);
+    expect(submission.score, 1);
+    expect(submission.selftext, '');
+    expect(submission.spam, isFalse);
+    expect(submission.spoiler, isFalse);
+    expect(submission.subredditType, 'restricted');
+    expect(submission.stickied, isFalse);
+    expect(
+        submission.title,
+        'DRAW: Using Dart to Moderate Reddit Comments'
+        ' (DartConf 2018)');
+    expect(
+        submission.thumbnail,
+        Uri.parse(
+            'https://a.thumbs.redditmedia.com/HP1keD9FPMG7fSBFZQk94HlJ4n13VB13yZagixx0wz4.jpg'));
+    expect(submission.upvoteRatio, 1.0);
+    expect(submission.upvotes, 1);
+    expect(submission.url,
+        Uri.parse('https://www.youtube.com/watch?v=VqNU_CYVaXg'));
+    expect(submission.viewCount, 16);
+    expect(submission.visited, isFalse);
+  });
+
+  test('lib/submission/refresh-comments-sanity', () async {
+    final reddit = await createRedditTestInstance(
+        'test/submission/lib_submission_refresh_comments_sanity.json');
+    final submission = await reddit.submission(id: '7x6ew7').populate();
+    final originalComments = submission.comments;
+    final updatedComments = await submission.refreshComments();
+    expect(submission.comments, updatedComments);
+    expect(originalComments.length, updatedComments.length);
+  });
+
   test('lib/submission/crosspost', () async {
     final reddit = await createRedditTestInstance(
       'test/submission/lib_submission_crosspost.json',
