e2910a248afc08808c071c36e893ff298e2b73f0
bkonyi <bkonyi@google.com>
2018-09-01 18:29:03 -0700

    Updated Travis and dart_coveralls config

---
+++ tool/travis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Install dart_coveralls; gather and send coverage data.
+if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
+  pub global activate dart_coveralls
+  # git clone https://github.com/bkonyi/dart-coveralls.git
+  # cd dart-coveralls
+  # pub get
+  # cd ..
+  echo "Running coverage..."
+  dart_coveralls report
+  #dart dart-coveralls/bin/dart_coveralls.dart report \
+    --retry 2 \
+    --exclude-test-files \
+    --throw-on-error \
+    --throw-on-connectivity-error \
+    --preview-dart-2 \
+    test/test_all.dart
+  echo "Coverage complete."
+  # rm -rf dart-coveralls
+else
+  if [ -z ${COVERALLS_TOKEN+x} ]; then echo "COVERALLS_TOKEN is unset"; fi
+  if [ -z ${TRAVIS_DART_VERSION+x} ]; then
+    echo "TRAVIS_DART_VERSION is unset";
+  else
+    echo "TRAVIS_DART_VERSION is $TRAVIS_DART_VERSION";
+  fi
+
+  echo "Skipping coverage for this configuration."
+fi
