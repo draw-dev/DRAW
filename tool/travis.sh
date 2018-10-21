da10a39468721fa953bf4e7d6962790c2d7815ea
bkonyi <bkonyi@google.com>
2018-10-20 17:10:56 -0700

    Attempt to fix coverage

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,14 +5,14 @@ set -e
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
-  pub global activate dart_coveralls 4.0.0
-  # git clone https://github.com/bkonyi/dart-coveralls.git
-  # cd dart-coveralls
-  # pub get
-  # cd ..
+  #pub global activate dart_coveralls
+  git clone https://github.com/bkonyi/dart-coveralls.git
+  cd dart-coveralls
+  pub get
+  cd ..
   echo "Running coverage..."
-  #dart dart-coveralls/bin/dart_coveralls.dart report \
-  dart_coveralls report \
+  dart dart-coveralls/bin/dart_coveralls.dart report \
+  #dart_coveralls report \
     --retry 2 \
     --exclude-test-files \
     --throw-on-error \
@@ -20,7 +20,7 @@ if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "dev" ]; then
     --preview-dart-2 \
     test/test_all.dart
   echo "Coverage complete."
-  # rm -rf dart-coveralls
+  rm -rf dart-coveralls
 else
   if [ -z ${COVERALLS_TOKEN+x} ]; then echo "COVERALLS_TOKEN is unset"; fi
   if [ -z ${TRAVIS_DART_VERSION+x} ]; then
