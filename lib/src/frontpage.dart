bee2bf08a65b1dbe0dd30ab1156981ffb322c5c4
ThatsEli <public@thatseliyt.de>
2019-07-30 21:30:59 +0200

    Added best sorting to frontpage (#160)

--- lib/src/frontpage.dart
+++ lib/src/frontpage.dart
@@ -1,9 +1,11 @@
-// Copyright (c) 2018, the Dart Reddit API Wrapper project authors.
+// Copyright (c) 2019, the Dart Reddit API Wrapper project authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
+import 'package:draw/draw.dart';
 import 'package:draw/src/base.dart';
+import 'package:draw/src/listing/listing_generator.dart';
 import 'package:draw/src/listing/mixins/base.dart';
 import 'package:draw/src/listing/mixins/gilded.dart';
 import 'package:draw/src/listing/mixins/rising.dart';
@@ -15,4 +17,15 @@ class FrontPage extends RedditBase
     with BaseListingMixin, GildedListingMixin, RisingListingMixin {
   String path = '/';
   FrontPage(Reddit reddit) : super(reddit);
+
+  /// Returns a [UserContent] that is "best".
+  ///
+  /// `limit` is the maximum number of objects returned by Reddit per request
+  /// (the default is 100). If provided, `after` specifies from which point
+  /// Reddit will return objects of the requested type. `params` is a set of
+  /// additional parameters that will be forwarded along with the request.
+  Stream<UserContent> best(
+          {int limit, String after, Map<String, String> params}) =>
+      ListingGenerator.createBasicGenerator(reddit, path + 'best',
+          limit: limit, after: after, params: params);
 }
--- test/frontpage/frontpage_test.dart
+++ test/frontpage/frontpage_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, the Dart Reddit API Wrapper project authors.
+// Copyright (c) 2019, the Dart Reddit API Wrapper project authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
@@ -18,6 +18,14 @@ Future<void> main() async {
     final reddit = await createRedditTestInstance(
         'test/frontpage/lib_frontpage_sanity.json');
     await for (final hot in reddit.front.hot(params: {'limit': '10'})) {
+      expect(hot is Submission, isTrue);
+    }
+  });
+
+  test('lib/frontpage/best', () async {
+    final reddit = await createRedditTestInstance(
+        'test/frontpage/lib_frontpage_best.json');
+    await for (final hot in reddit.front.best(params: {'limit': '10'})) {
       expect(hot is Submission, isTrue);
     }
   });
