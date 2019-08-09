96344a6dd2f5460abe3aced288ffae30d9d900ba
ThatsEli <public@thatseliyt.de>
2019-08-09 11:18:00 +0200

    updated copyright

--- lib/src/reddit.dart
+++ lib/src/reddit.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, the Dart Reddit API Wrapper project authors.
+// Copyright (c) 2019, the Dart Reddit API Wrapper project authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
