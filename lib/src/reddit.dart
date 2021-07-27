a0f0f05c21fdbfbf1f6ff6d2e4344f2a303f9281
Mohsin Sheikh <29257166+SupremeDeity@users.noreply.github.com>
2021-07-27 17:18:23 +0500

    Param 'raw_json' un-overridable.
    Param 'raw_json' will be applied, regardless of whether params are already passed or not.

--- lib/src/reddit.dart
+++ lib/src/reddit.dart
@@ -620,7 +620,8 @@ class Reddit {
           'Cannot make requests using unauthenticated client.');
     }
     final path = Uri.https(defaultOAuthApiEndpoint, api);
-    params ??= {'raw_json': '1'}; // only on null
+    params ??= {};
+    params['raw_json'] = '1';
     final response =
         await auth.get(path, params: params, followRedirects: followRedirects);
     return objectify ? _objector.objectify(response) : response;
